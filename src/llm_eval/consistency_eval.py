992907997f9addeeacc08b5cc3100156b2aaa73d
shumlh <tonyuglobe@gmail.com>
2024-05-21 12:06:59 -0700

    Update docstring and variable name for ConsistencyEvaluator

--- src/llm_eval/consistency_eval.py
+++ src/llm_eval/consistency_eval.py
@@ -2,11 +2,12 @@ import pandas as pd
 
 class ConsistencyEvaluator:
     def __init__(self):
-        self.evaluation_report = None
+        self.evaluation_reports = None
 
     def evaluate(self, models, num_test_runs=2, verbose=False):
         """
         Input the initialized TestEvaluator models, test run `num_test_runs` times to obtain the result
+        models = [{'name': 'model_no1', 'model': {{model object}}}, ...]
         """
         results = []
         for item in models:
@@ -26,14 +27,14 @@ class ConsistencyEvaluator:
                 result['model_name'] = item['name']
                 result['test_no'] = test_no+1
                 results.append(result)
-        self.evaluation_report = pd.DataFrame(results)
+        self.evaluation_reports = pd.DataFrame(results)
         return
 
     def get_completeness_score_dist(self):
         """
         Obtain the distribution of the Test Completeness scores
         """
-        completeness_score_df = self.evaluation_report.drop(columns='report')
+        completeness_score_df = self.evaluation_reports.drop(columns='report')
         completeness_score_df = completeness_score_df.pivot(index='model_name', columns='test_no', values='score')
         return completeness_score_df
 
@@ -42,10 +43,10 @@ class ConsistencyEvaluator:
         Obtain the distribution of the consistency per checklist item
         """
         consistency_df = pd.DataFrame()
-        for idx in self.evaluation_report.index:
-            result = self.evaluation_report.iloc[idx]['report'].reset_index()
-            result['test_no'] = self.evaluation_report.iloc[idx]['test_no']
-            result['model_name'] = self.evaluation_report.iloc[idx]['model_name']
+        for idx in self.evaluation_reports.index:
+            result = self.evaluation_reports.iloc[idx]['report'].reset_index()
+            result['test_no'] = self.evaluation_reports.iloc[idx]['test_no']
+            result['model_name'] = self.evaluation_reports.iloc[idx]['model_name']
             consistency_df = pd.concat([consistency_df, result], axis = 0, ignore_index=True)
         consistency_df = consistency_df.pivot(index=['model_name', 'ID'], columns=['test_no'], values=['is_Satisfied'])
         consistency_df.columns = consistency_df.columns.droplevel(level=0)
