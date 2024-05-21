7d066f1255f76f778856f52e8d4bec7878b2e16d
SoloSynth1 <solosynth1@gmail.com>
2024-05-21 12:14:24 -0700

    Merge branch 'main' into 74-handle-repos-with-no-test-cases

--- src/test_creation/modules/workflow/parse.py
+++ src/test_creation/modules/workflow/parse.py
@@ -5,17 +5,24 @@ class ResponseParser:
     def __init__(self, response):
         self.response = response
 
-    def get_completeness_score(self) -> str:
+    def get_completeness_score(self, score_format: str = 'fraction', verbose: bool = False) -> str:
         report_df = pd.DataFrame(self.response)['report'].explode('report').apply(pd.Series)
         report_df = report_df.groupby(['ID', 'Title']).agg({
             'Score': ['max', 'count'],
             'Functions': ['sum']
         })
         report_df.columns = ['is_Satisfied', 'n_files_tested', 'functions']
-        score = f"{report_df['is_Satisfied'].sum()}/{report_df['is_Satisfied'].count()}"
-        print("Report:")
-        print(report_df)
-        print()
-        print(f'Score: {score}')
-        print()
+        self.evaluation_report = report_df
+
+        if score_format == 'fraction':
+            score = f"{report_df['is_Satisfied'].sum()}/{report_df['is_Satisfied'].count()}"
+        elif score_format == 'number':
+            score = report_df['is_Satisfied'].sum()/report_df['is_Satisfied'].count()
+
+        if verbose:
+            print("Report:")
+            print(report_df)
+            print()
+            print(f'Score: {score}')
+            print()
         return score
