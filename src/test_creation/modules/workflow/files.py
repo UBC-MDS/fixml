f85f88b9367a30e6774c8b5688b0f8fe62eb7aff
SoloSynth1 <solosynth1@gmail.com>
2024-05-20 22:50:54 -0700

    refactor to incorporate more LangChain components into the pipeline

---
+++ src/test_creation/modules/workflow/files.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from ..code_analyzer.repo import Repository
+
+
+class RepoFileExtractor(ABC):
+    def __init__(self, repo: Repository):
+        self._repo = repo
+
+    @abstractmethod
+    def extract(self) -> list:
+        pass
+
+
+class PythonTestFileExtractor(RepoFileExtractor):
+    def __init__(self, repository: Repository):
+        super().__init__(repository)
+
+    def extract(self):
+        return self._repo.list_test_files()['Python']
---
+++ src/test_creation/modules/workflow/parse.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class ResponseParser:
+    def __init__(self, response):
+        self.response = response
+
+    def get_completeness_score(self):
+        report_df = pd.DataFrame(self.response)['report'].explode('report').apply(pd.Series)
+        report_df = report_df.groupby(['ID', 'Title']).agg({
+            'Score': ['max', 'count'],
+            'Functions': ['sum']
+        })
+        report_df.columns = ['is_Satisfied', 'n_files_tested', 'functions']
+        score = f"{report_df['is_Satisfied'].sum()}/{report_df['is_Satisfied'].count()}"
+        print("Report:")
+        print(report_df)
+        print()
+        print(f'Score: {score}')
+        print()
+        return score
