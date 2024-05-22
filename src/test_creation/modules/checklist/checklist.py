84662dee3a349e5178ed3acb63912cb3f5b6a157
John Shiu <asbjchk.academic@gmail.com>
2024-05-21 23:26:32 -0700

    fix: example in checklist.py

--- src/test_creation/modules/checklist/checklist.py
+++ src/test_creation/modules/checklist/checklist.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
         """Example calls. To be removed later.
 
         Example:
-        python src/checklist/checklist.py ./test-dump-csv/
+        python src/test_creation/modules/checklist/checklist.py ./checklist/test-dump-csv
 
         Note that the supplied path must be a directory containing 3 CSV files:
         1. `overview.csv`
