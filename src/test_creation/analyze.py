e33ec8c7254502bcb4ffbca7be9b50e5ebdf9c17
SoloSynth1 <solosynth1@gmail.com>
2024-05-27 17:13:56 -0700

    formatting; add `consistency_check.py` to store script for consistency checking

--- src/test_creation/analyze.py
+++ src/test_creation/analyze.py
@@ -1,7 +1,7 @@
 import fire
 
-import langchain
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
 from modules.workflow.prompt_format import EvaluationPromptFormat
 from modules.workflow.evaluator import PerFileTestEvaluator
@@ -9,11 +9,11 @@ from modules.checklist.checklist import Checklist, ChecklistFormat
 from modules.code_analyzer.repo import Repository
 from modules.workflow.parse import ResponseParser
 
-langchain.debug = True
+load_dotenv()
+
 
 if __name__ == '__main__':
     def main(checklist_path, repo_path):
-
         llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
         checklist = Checklist(checklist_path, checklist_format=ChecklistFormat.CSV)
         repo = Repository(repo_path)
@@ -23,6 +23,6 @@ if __name__ == '__main__':
         response = evaluator.evaluate()
 
         parser = ResponseParser(response)
-        parser.get_completeness_score()
+        parser.get_completeness_score(verbose=True)
 
     fire.Fire(main)
