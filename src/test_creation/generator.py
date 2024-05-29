a5937643d51ac8c68202a0d000253509f7aab342
John Shiu <asbjchk.academic@gmail.com>
2024-05-29 11:19:56 -0700

    refactored generator code

--- src/test_creation/generator.py
+++ src/test_creation/generator.py
@@ -22,6 +22,7 @@ from modules.workflow.parse import ResponseParser
 load_dotenv()
 
 
+# NOTE: only support Python
 class TestGenerator:
     def __init__(self, llm: LanguageModelLike, extractor: RepoFileExtractor = None, checklist: Checklist = None, retries: int = 3):
         self.llm = llm
@@ -30,43 +31,44 @@ class TestGenerator:
 
         self.retries = retries
 
-        # self.files = self.file_extractor.extract()
-        # if not self.files:
-        #     print("File loader returned no files!")
+        self.result = []
+
+        if extractor:
+            self.files = self.file_extractor.extract()
+            if not self.files:
+                print("File loader returned no files!")
 
         self.test_items = self._load_tests_from_checklist()
         if not self.test_items:
             print("Loaded checklist successfully, but it contains no test items!")
 
-        class TestSpecGeneration(BaseModel):
+        class TestCase(BaseModel):
             ID: str = Field(description="The corresponding `ID` of the checklist item provided")
             Title: str = Field(description="The corresponding `Title` of the checklist item provided")
-            Function: str = Field(description="A test function with the docstring of numpy format") # FIXME: define python function format
+            Function: str = Field(description="An empty test function with the docstring of numpy format") # FIXME: define numpy docstring format
 
-        class SpecGenResult(BaseModel):
-            results: List[TestSpecGeneration]
+        class TestCases(BaseModel):
+            results: List[TestCase]
 
-        self.parser = JsonOutputParser(pydantic_object=SpecGenResult)
+        self.parser = JsonOutputParser(pydantic_object=TestCases)
         
         self.prompt = PromptTemplate(
             template="You are an expert Machine Learning Engineer.\n"
-                     "Please generate Python test functions based on corresponding requirement of given checklist, with docstring of numpy format.\n"
+                     "Please generate empty Python test functions based on corresponding requirement of given checklist, with docstring of numpy format.\n"
                      "{format_instructions}\n" 
                      "Here is the checklist as a list of JSON objects:\n```{checklist}```\n",
                      #"Here is the code to be analyzed:\n{context}",
-            description="Test Generation for Machine Learning Project",
-            #input_variables=["checklist", "context"],
-            input_variables=["checklist"],
+            description="Test Specification Generation for Machine Learning Project",
+            input_variables=["checklist"], 
             partial_variables={"format_instructions": self.parser.get_format_instructions()},
         )
 
         self.chain = self.prompt | self.llm | self.parser
 
-        self.spec = []
 
     @staticmethod
     def _load_test_file_into_splits(file_path: str) -> List[Document]:
-        # FIXME: to examine whether this is still needed
+        # FIXME: it's not being used, but would be used when we feed in code base as context 
         loader = PythonLoader(file_path)
         py = loader.load()
         py_splits = RecursiveCharacterTextSplitter.from_language(language=Language.PYTHON, chunk_size=1000,
@@ -77,7 +79,7 @@ class TestGenerator:
         checklist = self.checklist.get_all_tests(['ID', 'Title', 'Requirement'])
         return json.dumps(checklist)
 
-    def generate_spec(self) -> List[dict]:
+    def generate(self) -> List[dict]:
         response = None
         retry_count = 0
         while not response and retry_count < self.retries:
@@ -91,48 +93,27 @@ class TestGenerator:
             raise RuntimeError(f"Unable to obtain valid response from LLM within {self.retries} attempts")
 
         result = response['results']
-        self.spec = result
+        self.result = result
             
         return result
 
-    def fill_functions_body(self) -> List[dict]:
-        prompt = PromptTemplate(
-            template="You are an expert Machine Learning Engineer.\n"
-                     "Please fill the Python test functions' body with docstring of numpy format.\n"
-                     "{format_instructions}\n" 
-                     "Here is a list of JSON objects, in which the 'Function's are the functions to be filled:\n```{functions}```\n",
-                     #"Here is the code to be analyzed:\n{context}",
-            description="Filling Test Functions for Machine Learning Project",
-            #input_variables=["checklist", "context"],
-            input_variables=["functions"],
-            partial_variables={"format_instructions": self.parser.get_format_instructions()},
-        )
-
-        chain = prompt | self.llm | self.parser
-
-        response = chain.invoke({"functions": json.dumps(self.spec)})
-        #result = response['results']
-
-        return response
+    def export_py(self, output_path: str, exist_ok: bool = False):
+        raise NotImplementedError()
 
 
 if __name__ == '__main__':
-    # FIXME: to be updated
-    def main(checklist_path, repo_path, report_output_path, report_output_format='html'):
+    def main(checklist_path):
         """
         Example:
         ----------
-        >>> python src/test_creation/analyze.py --checklist_path='./checklist/checklist_demo.csv' --repo_path='../lightfm/' --report_output_path='./report/evaluation_report.html' --report_output_format='html'
+        >>> python src/test_creation/generate.py --checklist_path='./checklist/checklist.csv' 
         """
         llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
         checklist = Checklist(checklist_path, checklist_format=ChecklistFormat.CSV)
-        extractor = PythonTestFileExtractor(Repository(repo_path))
 
-        evaluator = TestEvaluator(llm, extractor, checklist)
-        response = evaluator.evaluate()
+        generator = TestGenerator(llm, checklist=checklist)
+        result = generator.generate()
 
-        parser = ResponseParser(response)
-        parser.get_completeness_score(verbose=True)
-        parser.export_evaluation_report(report_output_path, report_output_format, exist_ok=True)
+        print(result)
 
     fire.Fire(main)
