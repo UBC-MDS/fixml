eaf8f367db23b2db0b1bc92cee5957cb415bcaf6
SoloSynth1 <solosynth1@gmail.com>
2024-05-29 18:31:55 -0700

    add installable executable `test-creation` with dummy calls

--- src/test_creation/test_creation.py
+++ src/test_creation/test_creation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -7,6 +8,10 @@ load_dotenv()
 
 
 def main():
+    """This is the CLI entry point."""
+    # TODO: Flesh out this! This is working on the older branch and refactoring had not been taken placed yet.
+    # TODO: Calls to analyze/export should be written here via Fire.
+
     llm = ChatOpenAI()
 
     prompt = ChatPromptTemplate.from_messages([
@@ -21,7 +26,3 @@ def main():
     output_str = chain.invoke({"input": "Hello LLM World! I can't wait to use you to write a test suite for my ML pipeline!"})
 
     print(output_str)
-
-
-if __name__ == "__main__":
-    main()
