4d2588f59eca81ee66ac8d94312bce2e681c709a
SoloSynth1 <solosynth1@gmail.com>
2024-06-03 15:47:05 -0700

    add CLI tool; add `src/cli`

--- src/test_creation/cli/checklist.py
+++ src/test_creation/cli/checklist.py
@@ -1,49 +1,27 @@
-from ..modules.checklist.checklist import Checklist, ChecklistFormat
+from ..modules.checklist.checklist import Checklist
 
 
 class ChecklistActions(object):
 
     @staticmethod
-    def convert(from_format, from_path, to_format, to_path, overwrite=False):
+    def convert(from_path: str, to_path: str, overwrite: bool = False) -> None:
         """Converts the checklist between all accepted formats.
 
         Parameters
         ----------
-        from_format
-            The format of the checklist to be converted.
         from_path
             The path to the checklist to be converted.
-        to_format
-            The format of the checklist after conversion.
         to_path
             The path to the converted checklist.
         overwrite
             The flag to bypass overwrite protection. This is by default False.
-
-        Returns
-        -------
-
         """
-        from_format = from_format.lower()
-        to_format = to_format.lower()
-        format_maps = {
-            'csv': ChecklistFormat.CSV,
-            'yaml': ChecklistFormat.YAML
-        }
-        if (from_format not in format_maps.keys() or to_format not in
-                format_maps.keys()):
-            raise ValueError(f"Invalid format(s) provided. The acceptable "
-                             f"formats are {list(format_maps.keys())}.")
-        checklist = Checklist(from_path, format_maps[from_format])
-        if to_format == "yaml":
-            checklist.to_yaml(to_path, no_preserve_format=True,
-                              exist_ok=overwrite)
-        elif to_format == "csv":
-            checklist.to_csv(to_path, exist_ok=overwrite)
+        checklist = Checklist(from_path)
+        checklist.write_to(to_path, exist_ok=overwrite)
         print(f"Done. Checklist saved to {to_path}.")
 
     @staticmethod
-    def export(from_format, from_path, to_format, to_path, overwrite=False):
+    def export(from_path: str, to_path: str, overwrite: bool = False) -> None:
         """Exports the checklist to a more human-readable report format.
 
         The exported checklist cannot be read back by the system.
@@ -52,23 +30,14 @@ class ChecklistActions(object):
 
         Parameters
         ----------
-        from_format
-            The format of the checklist to be converted.
         from_path
             The path to the checklist to be converted.
-        to_format
-            The format of the checklist after conversion.
         to_path
             The path to the converted checklist.
         overwrite
             The flag to bypass overwrite protection. This is by default False.
-
-        Returns
-        -------
-
-        Examples
-        --------
-        >>> test-creation checklist export csv ./checklist/checklist_demo.csv html ./report/checklist_report.html --overwrite
-
         """
+        checklist = Checklist(from_path)
+        checklist.export(to_path, exist_ok=overwrite)
+        print(f"Done. Checklist saved to {to_path}.")
 
