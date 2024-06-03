24b10ca8de6a219df0093904dcbdfb6f43b7d020
John Shiu <asbjchk.academic@gmail.com>
2024-06-03 16:56:16 -0700

    added back response writing feature

--- src/test_creation/cli/repository.py
+++ src/test_creation/cli/repository.py
@@ -1,3 +1,5 @@
+import pickle
+
 from langchain_openai import ChatOpenAI
 
 from ..modules.workflow.prompt_format import EvaluationPromptFormat
@@ -24,7 +26,8 @@ class RepositoryActions(object):
         pass
 
     @staticmethod
-    def evaluate(repo_path: str, report_output_path: str,
+    def evaluate(repo_path: str, report_output_path: str = None,
+                 response_output_path: str = None,
                  checklist_path: str = "./checklist/checklist.csv/",
                  model="gpt-3.5-turbo", verbose: bool = False,
                  overwrite: bool = False) -> None:
@@ -40,10 +43,13 @@ class RepositoryActions(object):
         repo_path
             The path of the git repository to be analyzed.
         report_output_path
-            The path which the evaluation will be written to. The format of
-            the evaluation report will be based on the extension provided in
-            this path. The extensions must be either one of `.html`, `.htm`,
-            `.pdf`, or `.qmd`.
+            Optional. If provided, the system will write the evaluation to
+            the path. The format of the evaluation report will be based on
+            the extension provided in this path. The extensions must be
+            either one of `.html`, `.htm`, `.pdf`, or `.qmd`.
+        response_output_path
+            Optional. If provided, the system will write the response in
+            PICKLE to the path.
         checklist_path
             Optional flag to use non-default checklist during the operation.
         model
@@ -64,8 +70,12 @@ class RepositoryActions(object):
         evaluator = PerFileTestEvaluator(llm, prompt_format=prompt_format,
                                          repository=repo, checklist=checklist)
         response = evaluator.run()
+        if response_output_path:
+            with open(response_output_path, 'wb') as file:
+                pickle.dump(response, file)
 
-        parser = ResponseParser(response)
-        parser.get_completeness_score(verbose=verbose)
+        if report_output_path:
+            parser = ResponseParser(response)
+            parser.get_completeness_score(verbose=verbose)
 
-        parser.export_evaluation_report(report_output_path, exist_ok=overwrite)
+            parser.export_evaluation_report(report_output_path, exist_ok=overwrite)
