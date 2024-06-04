93160a76d1c747a193accfb5fa22100ff28d33cc
SoloSynth1 <solosynth1@gmail.com>
2024-06-04 16:16:43 -0700

    include basic tests for gitcontext; improve tests by parametrizing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,31 +1,21 @@
 from pathlib import Path
 
+import pytest
 from test_creation.modules.utils import get_extension
 
 
-def test_extension_from_string_can_be_extracted_correctly():
-    correct_path = "checklist/checklist.csv"
-    assert get_extension(correct_path) == "csv"
-
-
-def test_extension_from_path_can_be_extracted_correctly():
-    correct_path = Path("checklist/checklist.csv")
-    assert get_extension(correct_path) == "csv"
-
-
-def test_extension_extracted_is_all_lower_cased():
-    path = "ALL/CAPITAL/PATH/TEST.ZIP"
-    assert get_extension(path) == "zip"
-
-
-def test_only_last_extension_will_be_extracted():
-    path = "test/multi_ext.tar.gz"
-    assert get_extension(path) == "gz"
-
-
-def test_file_with_no_extension_will_produce_empty_string():
-    path = "test/README"
-    assert get_extension(path) == ""
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("checklist/checklist.csv", "csv"),
+        (Path("checklist/checklist.csv"), "csv"),
+        ("ALL/CAPITAL/PATH/TEST.ZIP", "zip"),
+        ("test/multi_ext.tar.gz", "gz"),
+        (Path("test/README"), "")
+    ]
+)
+def test_extension_from_string_can_be_extracted_correctly(path, expected):
+    assert get_extension(path) == expected
 
 
 def test_extracted_extension_does_not_start_with_dot():
