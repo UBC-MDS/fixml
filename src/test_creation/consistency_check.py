f1e1eaf0b1414cd4aee54e88261a0a4e7245d2da
SoloSynth1 <solosynth1@gmail.com>
2024-06-05 14:09:39 -0700

    change relative import style conforming to PEP 328

--- src/test_creation/consistency_check.py
+++ src/test_creation/consistency_check.py
@@ -2,18 +2,18 @@ import fire
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
-from modules.workflow.prompt_format import EvaluationPromptFormat
-from modules.workflow.evaluator import PerFileTestEvaluator
-from modules.checklist.checklist import Checklist, ChecklistFormat
-from modules.code_analyzer.repo import Repository
-from modules.llm_eval.consistency_eval import ConsistencyEvaluator
+from .modules.workflow.prompt_format import EvaluationPromptFormat
+from .modules.workflow.runners.evaluator import PerFileTestEvaluator
+from .modules.checklist.checklist import Checklist, ChecklistFormat
+from .modules.code_analyzer.repo import Repository
+from .modules.llm_eval.consistency_eval import ConsistencyEvaluator
 
 load_dotenv()
 
 
 if __name__ == '__main__':
     def main(checklist_path, repo_path):
-        checklist = Checklist(checklist_path, checklist_format=ChecklistFormat.CSV)
+        checklist = Checklist(checklist_path)
         repo = Repository(repo_path)
         prompt_format = EvaluationPromptFormat()
 
