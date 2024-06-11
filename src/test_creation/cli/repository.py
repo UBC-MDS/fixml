616411519220615b4a82787799c8c16064368aba
SoloSynth1 <solosynth1@gmail.com>
2024-06-10 23:44:11 -0700

    update `test-creation evaluate` to dump JSON file

--- src/test_creation/cli/repository.py
+++ src/test_creation/cli/repository.py
@@ -60,8 +60,8 @@ class RepositoryActions(object):
                 file.write("\n\n")
 
     @staticmethod
-    def evaluate(repo_path: str, report_output_path: str = None,
-                 response_output_path: str = None,
+    def evaluate(repo_path: str, save_to: str = None,
+                 render_report_to: str = None,
                  checklist_path: str = "./checklist/checklist.csv/",
                  model="gpt-3.5-turbo", verbose: bool = False,
                  overwrite: bool = False, debug: bool = False,
@@ -71,20 +71,22 @@ class RepositoryActions(object):
         This will evaluate the completeness of the test suites given a git
         repository, based on a checklist which consists of items relevant to
         various aspects related to Data Science/Machine Learning projects. A
-        report will be generated according to the given format specified in
+        JSON file containing the evaluation results would be saved. Optionally,
+        you can provide the `render_report_to` flag to use the content of the
+        JSON file to render a human-readable report in HTML or PDF format.
 
         Parameters
         ----------
         repo_path
             The path of the git repository to be analyzed.
-        report_output_path
-            Optional. If provided, the system will write the evaluation to
-            the path. The format of the evaluation report will be based on
-            the extension provided in this path. The extensions must be
+        save_to
+            Optional. If provided, the JSON file saved would be in the specified
+            path instead of the default location.
+        render_report_to
+            Optional. If provided, the system will render the evaluation report
+            to the specified path. The format of the evaluation report will be
+            based on the extension provided in this path. The extensions must be
             either one of `.html`, `.htm`, `.pdf`, or `.qmd`.
-        response_output_path
-            Optional. If provided, the system will write the response in
-            PICKLE to the path.
         checklist_path
             Optional flag to use non-default checklist during the operation.
         test_dirs
@@ -115,16 +117,27 @@ class RepositoryActions(object):
         evaluator = PerFileTestEvaluator(llm, prompt_format=prompt_format,
                                          repository=repo, checklist=checklist,
                                          test_dirs=parsed_test_dirs)
-        response = evaluator.run()
-        if response_output_path:
-            json_str = response.model_dump_json()
-            with open(response_output_path, 'w') as f:
-                f.write(json_str)
+        response = evaluator.run(verbose=verbose)
 
-        if report_output_path:
+        if not save_to:
+            repo_name = response.repository.object.root.stem
+            commit_hash = response.repository.git_commit
+            eval_time = response.call_results[0].start_time.timestamp()
+            response_output_path = Path(
+                f"./evaluation_{repo_name}_{commit_hash}_{eval_time:0.0f}.json"
+            ).resolve()
+        else:
+            response_output_path = Path(save_to).resolve()
+
+        response.to_json(output_path=response_output_path, exist_ok=overwrite)
+        print(f"Evaluation response saved to {response_output_path}.")
+
+        if render_report_to:
             parser = ResponseParser(response)
             parser.get_completeness_score(verbose=verbose)
-            parser.export_evaluation_report(report_output_path, exist_ok=overwrite)
+            parser.export_evaluation_report(render_report_to,
+                                            exist_ok=overwrite)
+            print(f"Evaluation report exported to {render_report_to}.")
 
     @staticmethod
     def list_tests(repo_path: str, test_dirs: list[Union[str, Path]] = None):
